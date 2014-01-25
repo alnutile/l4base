95ccb9c2a5b4c29e9fabe24d5136ba9c01a3c742
alnutile <alfrednutile@gmail.com>
2014-01-24 23:15:22 -0500

    Clean out controller

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -8,11 +8,6 @@ class UsersController extends BaseController {
         $this->beforeFilter('csrf', array('on' => 'post'));
     }
 
-
-    public function getNewaccount() {
-        return View::make('users.newaccount');
-    }
-
 	/**
 	 * Display a listing of the resource.
 	 *
@@ -20,7 +15,7 @@ class UsersController extends BaseController {
 	 */
 	public function index()
 	{
-        return View::make('users.index');
+        //return View::make('users.index');
 	}
 
 	/**
@@ -30,47 +25,10 @@ class UsersController extends BaseController {
 	 */
 	public function create()
 	{
-        $validator = Validator::make(Input::all(), User::$rules);
 
-        if($validator->passes()) {
-            //@TODO use the model method to just pass
-            //  the input values to the model and not do all of this
-            //  and move it to the model for validation in there
-            $user = new User;
-            $user->firstname = Input::get('firstname');
-            $user->lastname = Input::get('lastname');
-            $user->email = Input::get('email');
-            $user->password = Hash::make(Input::get('password'));
-            $user->firstname = Input::get('firstname');
-            $user->save();
-            //@TODO they should auto login at this point
-            return Redirect::to('users/signin')
-                ->with('message', 'Thank you for signing up please login');
-        }
-
-        return Redirect::to('users.newaccount')
-            ->with('message', 'Error in signing up')
-            ->withErrors($validator)
-            ->withInput();
         //return View::make('users.create');
     }
 
-    public function getSignin() {
-        return View::make('users.signin');
-    }
-
-    public function postSignin() {
-        if(Auth::attempt(array('email' => Input::get('email'), 'password' => Input::get('password')))) {
-            return Redirect::to('/')->with('message', 'You are signed in');
-        }
-
-        return Redirect::to('users/signin')->with('message', 'You email or password was incorrect');
-    }
-
-    public function getSignout(){
-        Auth::logout();
-        return Redirect::to('users/signin');
-    }
 
 	/**
 	 * Store a newly created resource in storage.
